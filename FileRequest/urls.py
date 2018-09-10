4c82b3f352ff4bcbef767bf2f21bab3e4cd72470
lucienshui <18952904@qq.com>
2018-09-11 01:34:24 +0800

    remove the old one

--- FileRequest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='fileRequestHome'),
-    path('submit', views.submit, name='fileRequestSubmit'),
-    path('download', views.download, name='fileRequestDownload'),
-]
--- Index/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(requests):
-    return render(requests, 'Index/index.html')

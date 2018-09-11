ce75aa734c0291e3b5e8d72160eb999e26c9335d
zhaort <32431336+zhaort@users.noreply.github.com>
2018-09-11 23:09:08 +0800

    Update lib.py

--- Server/Upload/lib.py
+++ Server/Upload/lib.py
@@ -1,4 +1,13 @@
-def unique(fileName):  # fileName: 传入的文件名
+def unique(fileName,content):  # fileName: 传入的文件名
+    def getHash(f):
+        line=f.readline()
+        hash=hashlib.md5()
+        while(line):
+            hash.update(line)
+            line=f.readline()
+        return hash.hexdigest()
+    
+    fileName=getHash(content)
     return fileName  # return 绝对唯一的文件名
 
 
